323dbc30a0f5756ee1e77f936829432cc4b266be
Matoyak <mwilliam09@gmail.com>
2019-03-04 23:35:30 -0600

    tuts part 1 and 2

---
+++ src/main/java/com/matoyak/suppergerrie/Reference.java
@@ -0,0 +1,8 @@
+package com.matoyak.suppergerrie;
+
+public class Reference {
+	public static final String MODID = "suppergerrie_tut";
+	public static final String MODNAME = "Supper Gerrie Tutorial Mod";
+	public static final String VERSION = "0.1";
+	public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12,)";
+}
